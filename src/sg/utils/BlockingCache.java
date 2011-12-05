0083f3ac6e406937cae613d2c290ee1913e4b9ff
Satyendra Gurjar <satyendra@sgurjar.com>
2011-12-05 15:03:35 -0500

    updates

--- src/sg/utils/BlockingCache.java
+++ src/sg/utils/BlockingCache.java
@@ -9,85 +9,74 @@ Requirements:
  1 only one instance per key should be created, either because its absent
    for a given key or its expired (in that case it will be recreated).
  2 most common path asking for a value for a key should not block or synchronize.
- 3 when a value doesnt exists for a key, one thread should create the instance
-   of value and put in the cache, other threads asking for same key should wait
-   till thats complete.
- 4 if a value is expired, one thread should (re)create the instance of value
-   and other threads asking for same key (when one thread is recreating it)
-   should BLOCK untill new value is created.
+ 3 when a value doesnt exists for a key, one thread should create the instance of
+   value and put in the cache, other threads asking for same key should wait till thats complete.
+ 4 if a value is expired, one thread should (re)create the instance of value and other threads
+   asking for same key (when one thread is recreating it) should BLOCK untill new value is created.
+ http://cs.oswego.edu/pipermail/concurrency-interest/2011-October/008335.html
+*/
 
-   http://cs.oswego.edu/pipermail/concurrency-interest/2011-October/008335.html
- */
+public class BlockingCache<K, V> implements Cache<K, V> {
 
-public class BlockingCache<K,V> implements Cache<K, V> {
-    private final CacheEntryFactory<K,V> factory;
+    private static final String ERR_CREATING_FIRST_INSTANCE =
+        "This thread was waiting for first instance creation for the key '%s', " +
+        "first instance creation has failed.";
+
+    private final CacheEntryFactory<K, V> factory;
     private final EvictionPolicy<V> evictionpolicy;
-    private final ConcurrentMap<K, Object> store = new ConcurrentHashMap<K, Object>(); // ensures publication
+    private final ConcurrentMap<K, Object> store = new ConcurrentHashMap<K, Object>();
 
-    public BlockingCache(
-            final CacheEntryFactory<K, V> factory,
-            final EvictionPolicy<V> evictionpolicy
-        ) {
-            this.factory = factory;
-            this.evictionpolicy = evictionpolicy;
-        }
+    public BlockingCache(final CacheEntryFactory<K, V> factory,
+                         final EvictionPolicy<V> evictionpolicy) {
+        this.factory = factory;
+        this.evictionpolicy = evictionpolicy;
+    }
 
     @SuppressWarnings("unchecked")
     @Override
-    public
-    V get(final K key) {
+    public V get(final K key) {
         Object ret = this.store.get(key);
 
         if (ret == null) {
             final CountDownLatch latch = new CountDownLatch(1);
             final Object prev = this.store.putIfAbsent(key, latch);
 
-			if (null == prev) {
-				try {
-					// this might throw runtimeexception
-					ret = this.factory.create(key);
-					this.store.replace(key, ret);
-				} catch (final Throwable t) {
-					this.store.remove(key);
-					throw (t instanceof RuntimeException) ? (RuntimeException) t
-							: new RuntimeException(t);
-				} finally {
-					latch.countDown();
-				}
-			} else {
-				ret = prev;
-			}
+            if (null == prev) {
+                try {
+                    ret = this.factory.create(key);
+                    this.store.replace(key, ret);
+                } catch (final Throwable t) {
+                    this.store.remove(key);
+                    throw (t instanceof RuntimeException) ? (RuntimeException) t : new RuntimeException(t);
+                } finally {
+                    latch.countDown();
+                }
+            } else {
+                ret = prev;
+            }
         }
 
         if (ret instanceof CountDownLatch) {
-            // no instance is created yet for this key, it is being created
-            // by another thread, wait till thats done
-            try {
-                ((CountDownLatch) ret).await();
-            } catch (final InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            final CountDownLatch latch = (CountDownLatch) ret;
 
-            // this is newly created instance of this key
-            // returned value can be null if it cant create key first-time
+            try { latch.await(); }
+            catch (final InterruptedException e) { throw new RuntimeException(e); }
+
             ret = this.store.get(key);
 
-            if (ret == null) {
-				throw new RuntimeException(
-						"error in creating first instance for key '" + key
-								+ "', please check logs");
-			}
-        }
-        else {
-            final V entry = (V) ret;
-
-            if (this.evictionpolicy != null && this.evictionpolicy.isExpired(entry)) {
-				this.store.remove(key, entry);
-                // so that next get will get updated value
-                // and all threads requesting for same key will BLOCK.
-            }
+            if (ret == null || ret instanceof CountDownLatch)
+                throw new RuntimeException(String.format(ERR_CREATING_FIRST_INSTANCE, key));
         }
 
-        return (V)ret;
+        final V entry = (V) ret;
+
+        if (this.evictionpolicy != null && this.evictionpolicy.isExpired(entry)) {
+            this.store.remove(key, entry);
+            // so that one of the next threads) asking for value for same key will
+            // go thru first time creation flow, and rest of the threads will block
+            // till the creation completes, successfully or unsuccessfully.
+        }
+
+        return entry;
     }
 }
--- src/sg/utils/SimpleCache.java
+++ src/sg/utils/SimpleCache.java
@@ -22,86 +22,76 @@ Requirements:
  */
 public class SimpleCache<K,V> implements Cache<K,V> {
 
-    private final CacheEntryFactory<K,V> factory;
+    private final CacheEntryFactory<K, V> factory;
     private final EvictionPolicy<V> evictionpolicy;
-    private final ConcurrentMap<K, Object> store = new ConcurrentHashMap<K, Object>(); // ensures publication
+    private final ConcurrentMap<K, Object> store = new ConcurrentHashMap<K, Object>();
 
-    public SimpleCache(
-        final CacheEntryFactory<K, V> factory,
-        final EvictionPolicy<V> evictionpolicy
-    ) {
+    private static final String ERR_CREATING_FIRST_INSTANCE =
+        "This thread was waiting for first instance creation for the key '%s', " +
+        "first instance creation has failed.";
+
+    public SimpleCache(final CacheEntryFactory<K, V> factory,
+                       final EvictionPolicy<V> evictionpolicy) {
         this.factory = factory;
         this.evictionpolicy = evictionpolicy;
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public
-    V get(final K key) {
+    public V get(final K key) {
         Object ret = this.store.get(key);
 
         if (ret == null) {
             final CountDownLatch latch = new CountDownLatch(1);
-            final Object prev = this.store.putIfAbsent(key, latch);
+            ret = this.store.putIfAbsent(key, latch);
 
-            if (null == prev) {
-				try {
-					// this might throw runtimeexception
-					ret = new Entry<V>(this.factory.create(key));
-					this.store.replace(key, ret);
-				} catch (final Throwable t) {
-					this.store.remove(key);
-					throw (t instanceof RuntimeException) ? (RuntimeException) t
-							: new RuntimeException(t);
-				} finally {
-					latch.countDown();
-				}
-            } else {
-                ret = prev;
+            if (ret == null) {
+                try {
+                    final Entry<V> entry = new Entry<V>(this.factory.create(key));
+                    this.store.replace(key, entry);
+                    ret = entry;
+                } catch(final Throwable t) {
+                    this.store.remove(key);
+                    throw (t instanceof RuntimeException) ? (RuntimeException) t : new RuntimeException(t);
+                } finally {
+                    latch.countDown();
+                }
             }
         }
 
         if (ret instanceof CountDownLatch) {
-            // no instance is created yet for this key, it is being created
-            // by another thread, wait till thats done
-            try {
-                ((CountDownLatch) ret).await();
-            } catch (final InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            final CountDownLatch latch = (CountDownLatch) ret;
 
-            // this is newly created instance of this key
+            try { latch.await(); }
+            catch (final InterruptedException e) { throw new RuntimeException(e); }
+
             ret = this.store.get(key);
 
-			if (ret == null) {
-				throw new RuntimeException(
-						"error in creating first instance for key '" + key
-								+ "', please check logs");
-			}
-        } else {
-            Entry<V> entry = (Entry<V>) ret;
+            if (!(ret instanceof Entry)) {
+                throw new RuntimeException(String.format(ERR_CREATING_FIRST_INSTANCE, key));
+            }
+        }
 
-            if (this.evictionpolicy != null && this.evictionpolicy.isExpired(entry.v)) {
-                final AtomicInteger sync = entry.sync;
+        Entry<V> entry = (Entry<V>) ret;
 
-                if (sync.compareAndSet(0, 1)) { // lock
-                    // multiple threads can return true from canEvict
-                    // but only one can get hold of lock, threads that can't
-                    // get hold of lock doesn't block they returns old value
-                    // while new value is being baked.
-                    try {
-                        entry = (Entry<V>) this.store.get(key);
+        if (this.evictionpolicy != null && this.evictionpolicy.isExpired(entry.v)) {
+            final AtomicInteger sync = entry.sync;
 
-                        if (this.evictionpolicy.isExpired(entry.v)) { // double-check
-                            final Entry<V> newval = new Entry<V>(this.factory.create(key), sync);
-                            this.store.replace(key, newval); // replace might be faster then put
-                            ret = newval;
-                        }
-                    } catch (final Throwable catchall) {
-                        catchall.printStackTrace(System.err); // old value will be returned
-                    } finally {
-                        sync.set(0); // don't forget to unlock
+            if (sync.compareAndSet(0, 1)) {
+                try {
+                    entry = (Entry<V>) this.store.get(key);
+
+                    if (this.evictionpolicy.isExpired(entry.v)) { // double-check
+                        // here we are using same 'sync' instance as it was in expired entry
+                        // so that there is always one lock per entry.
+                        final Entry<V> newval = new Entry<V>(this.factory.create(key), sync);
+                        this.store.replace(key, newval);
+                        ret = newval;
                     }
+                } catch (final Throwable catchall) {
+                    catchall.printStackTrace();
+                } finally {
+                    sync.set(0);
                 }
             }
         }
@@ -109,11 +99,17 @@ public class SimpleCache<K,V> implements Cache<K,V> {
         return ((Entry<V>) ret).v;
     }
 
-    private static final
-    class Entry<V> {
+    private static final class Entry<V> {
         final AtomicInteger sync;
         final V v;
-        Entry(final V v) { this(v, new AtomicInteger(0)); }
-        Entry(final V v, final AtomicInteger sync) { this.v = v; this.sync = sync; }
-   }
+
+        Entry(final V v) {
+            this(v, new AtomicInteger(0));
+        }
+
+        Entry(final V v, final AtomicInteger sync) {
+            this.v = v;
+            this.sync = sync;
+        }
+    }
 }
